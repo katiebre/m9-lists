0b46f9a8586ac58dc15e326c2544d69e71f062c3
KATIE BRELAND <KATIEBRE@UW.EDU>
2017-04-06 14:29:20 -0700

    completed the exercise

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,36 +1,32 @@
-# Exercise 1: Lists
+# Exercise 3: Vector and function practice
 
-# Create a vector of everything you ate for breakfast
+# Create a vector `marbles` with 6 different colors in it (representing marbles)
+marbles <- c('red','orange','yellow','green','blue','purple' )
 
+# Use the `sample` function to select a single marble
+sample(marbles, size = 1)
 
-# Create a vector of everything you ate for lunch
-
-
-# Create a list "meals" that has contains your `breakfast` and `lunch`
-
-
-# Add a "dinner" element to your "meals" list that has what you plan to eat for dinner
-
-
-# Use dollar notation to extract your `dinner` element from your list
-# and save it in a vector called 'dinner'
-
-
-# Use double-bracket notation to extract your `lunch` element from your list
-# and save it in your list as the element at index 5 (no reason)
-
-
-# Use single-bracket notation to extract your breakfast and lunch from your list
-# and save them to a list called `early.meals`
+# Write a function MarbleGame that does the following:
+# - Takes in a `guess` of a marble color
+# - Randomly samples a marble
+# - Returns whether or not the person guessed accurately (preferrably a full phrase)
+MarbleGame <- function(guess){
+  marble <- sample(marbles, 1)
+  if(marble == guess){
+  return ('you win!')
+  } else {
+  return ('you loose:(')
+  }
+}
+MarbleGame('blue')
 
 
 
-### Bonus ###
-# Create a list that has the number of items you ate for each meal
-# Hint: use the `lappy()` function
+# Play the marble game!
 
 
-# Write a function that adds pizza to every meal
+# Bonus: Play the marble game until you win, keeping track of how many tries you take
 
 
-# Add pizza to every meal!
+## Double bonus(answer not provided): play the game 1000X (until you win) and track the average number of tries
+# Is it what you expected based on the probability
