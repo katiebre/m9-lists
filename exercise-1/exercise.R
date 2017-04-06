3143ad4f0aa1419e7974c06f281117a05afc4e29
KATIE BRELAND <KATIEBRE@UW.EDU>
2017-04-06 14:40:43 -0700

    participated in the exexercise

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,32 +1,40 @@
-# Exercise 3: Vector and function practice
+# Exercise 1: Lists
 
-# Create a vector `marbles` with 6 different colors in it (representing marbles)
-marbles <- c('red','orange','yellow','green','blue','purple' )
+# Create a vector of everything you ate for breakfast
+my.breakfast <- c('juice', 'creamcheese', 'bagel')
 
-# Use the `sample` function to select a single marble
-sample(marbles, size = 1)
+# Create a vector of everything you ate for lunch
+my.lunch <- c('salad', 'sandwhich')
 
-# Write a function MarbleGame that does the following:
-# - Takes in a `guess` of a marble color
-# - Randomly samples a marble
-# - Returns whether or not the person guessed accurately (preferrably a full phrase)
-MarbleGame <- function(guess){
-  marble <- sample(marbles, 1)
-  if(marble == guess){
-  return ('you win!')
-  } else {
-  return ('you loose:(')
-  }
+# Create a list "meals" that has contains your `breakfast` and `lunch`
+meals <- list(breakfast = my.breakfast, lunch = my.lunch)
+
+# Add a "dinner" element to your "meals" list that has what you plan to eat for dinner
+meals$dinner <- c('rice', 'chicken')
+
+# Use dollar notation to extract your `dinner` element from your list
+# and save it in a vector called 'dinner'
+dinner <- meals$dinner
+
+# Use double-bracket notation to extract your `lunch` element from your list
+# and save it in your list as the element at index 5 (no reason)
+meals[[5]] <- meals[['lunch']]
+
+# Use single-bracket notation to extract your breakfast and lunch from your list
+# and save them to a list called `early.meals`
+
+
+
+### Bonus ###
+# Create a list that has the number of items you ate for each meal
+# Hint: use the `lappy()` function
+items <- lapply(meals, length)
+
+# Write a function that adds pizza to every meal
+AddPizza <- function(meals) {
+  x <- c(x, 'pizza')
 }
-MarbleGame('blue')
 
+# Add pizza to every meal!
+lapply(meals, AddPizza)
 
-
-# Play the marble game!
-
-
-# Bonus: Play the marble game until you win, keeping track of how many tries you take
-
-
-## Double bonus(answer not provided): play the game 1000X (until you win) and track the average number of tries
-# Is it what you expected based on the probability
